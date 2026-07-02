153fe5adb51863db086e54e27acca37449abe601
Iqbal Ahmed <vuttuv00@gmail.com>
2026-07-02 13:00:45 +0600

    Add event listeners for login and admin buttons

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+console.log("NCE Digital Routine v2 Loaded");
+
+const loginBtn = document.querySelector(".top-menu button:first-child");
+const adminBtn = document.querySelector(".top-menu button:last-child");
+
+if(loginBtn){
+loginBtn.onclick = () => {
+window.location.href = "admin/login.html";
+};
+}
+
+if(adminBtn){
+adminBtn.onclick = () => {
+window.location.href = "admin/dashboard.html";
+};
+}
+
+});
