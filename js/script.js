ea94095689eac73c9be130193123c4b4f0de9d33
Iqbal Ahmed <jsi.iqbal@gmail.com>
2026-07-19 22:27:37 +0600

    Confirmed JS logic to show full weekly routine

--- js/script.js
+++ js/script.js
@@ -3,15 +3,17 @@ fetch('data/routine.json')
     .then(data => {
         const tbody = document.getElementById('ece-body');
         tbody.innerHTML = '';
-        data.NCE_Routine.forEach(item => {
-            const row = `<tr>
-                <td>${item.Day}</td>
-                <td>${item['08:00']}</td>
-                <td>${item['09:00']}</td>
-                <td>${item['10:00']}</td>
-                <td>${item['11:00']}</td>
-            </tr>`;
-            tbody.innerHTML += row;
-        });
+        if (data.NCE_Digital_Routine && data.NCE_Digital_Routine.length > 0) {
+            data.NCE_Digital_Routine.forEach(item => {
+                const row = `<tr>
+                    <td>${item.Day}</td>
+                    <td>${item['08:00']}</td>
+                    <td>${item['09:00']}</td>
+                    <td>${item['10:00']}</td>
+                    <td>${item['11:00']}</td>
+                </tr>`;
+                tbody.innerHTML += row;
+            });
+        }
     })
     .catch(error => console.error('Error loading JSON:', error));
